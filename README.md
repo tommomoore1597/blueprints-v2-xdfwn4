c7b4b4c5db711ca37e29bd2769543a48d05798c5
tommomoore1597 <thomasmoore1597@gmail.com>
2022-04-06 09:11:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# blueprints-v2-xdfwn4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/blueprints-v2-xdfwn4)
---
+++ index.js
@@ -0,0 +1,6 @@
+// Import stylesheets
+import './styles/styles.scss';
+
+// Write Javascript code!
+// const appDiv = document.getElementById('app');
+// appDiv.innerHTML = `<h1>JS Starter</h1>`;
